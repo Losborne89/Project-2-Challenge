a4c0cd05d2b01343cf344ac6c1a481907e8ed504
Losborne89 <145294069+Losborne89@users.noreply.github.com>
2023-09-30 16:51:34 +0100

    Play Fetch Tutorial
    - play fetch tutorial completed
    -bonus sections added

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    private bool canSpawnDog = true;
+
     public GameObject dogPrefab;
+    private float spawnDelay = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canSpawnDog)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnDog();
+                Invoke(nameof(ResetSpawnDog), spawnDelay);
+            } 
         }
+        
+    }
+    void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        canSpawnDog = false;
+    }
+    private void ResetSpawnDog()
+    {
+        canSpawnDog = true;
     }
 }
